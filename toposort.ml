23e843405aaada935d4662b2ee8ad5e68da7b689
amwms <ania.stawiska@gmail.com>
2021-01-01 20:10:31 +0100

    toposort not only for int values - with debugs

--- toposort.ml
+++ toposort.ml
@@ -3,16 +3,82 @@
 (* exeption if the graph given is not a DAG *)
 exception Cykliczne;;
 
+(* convertion of 'a graph to int graph *)
+(* creating a maps with int and 'a as keys for vertecies of graph given as graph_list
+    and counting the number of vertecies in the given graph *)
+let rec convert_to_map graph_list graph_map graph_map2 n =
+  match graph_list with
+  | [] -> (graph_map, graph_map2, n)
+  | (v, edges) :: tail ->
+    let rec pom l g_map g_map2 pomn =
+      match l with
+      | [] -> (g_map, g_map2, pomn)
+      | h :: t ->
+        if not (PMap.exists h g_map2) then
+          pom t (PMap.add (pomn + 1) h g_map) (PMap.add h (pomn + 1) g_map2) (pomn + 1)
+        else
+          pom t g_map g_map2 pomn
+    in
+    if not (PMap.exists v graph_map2) then
+      let (g_m, g_m2, pom_n) =
+        pom edges (PMap.add (n + 1) v graph_map) (PMap.add v (n + 1) graph_map2) (n + 1) in
+      convert_to_map tail g_m g_m2 pom_n
+    else
+      let (g_m, g_m2, pom_n) =
+        pom edges graph_map graph_map2 n in
+      convert_to_map tail g_m g_m2 pom_n
+;;
+
+let rec convert_to_int_list lst graph_map acc =
+  match lst with
+  | [] -> acc
+  | h :: t ->
+    convert_to_int_list t graph_map ((PMap.find h graph_map) :: acc)
+;;
+
+(* converting 'a graph given as a list to int graph
+   represented as an array of arrays *)
+let convert graph_list graph_map graph n =
+  let graph_lst = ref graph_list in
+  for i = 0 to n - 1 do
+    let (v, edges) = List.hd !graph_lst in
+    let int_edges = convert_to_int_list edges graph_map [] in
+    let v = PMap.find v graph_map in
+    graph.(v) <- int_edges;
+    graph_lst := List.tl !graph_lst;
+  done;
+  ();;
+
+(* convert int vertecies back to original 'a values *)
+let rec decode int_list a_list graph_map =
+  match int_list with
+  | [] -> a_list
+  | h :: t -> decode t ((PMap.find h graph_map) :: a_list) graph_map
+;;
+
+
+(* number of vertecies in a graph given as a list
+   eg.: [(1, [2; 3]); (2, [4]); (3, [4; 5])]*)
+(* let rec num_of_vertecies lst n =
+  match lst with
+  | [] -> n
+  | (v, edges) :: tail ->List.rev (
+    let rec pom l maxi =
+      match l with
+      | [] -> maxi
+      | h :: t -> pom t (max maxi h) in
+    num_of_vertecies tail (max n (pom edges v));; *)
+
 (* converting given list representing a graph
    to an array of arrays graph representation *)
-let create_graph n graph_list graph =
+(* let create_graph n graph_list graph =
   let g = Array.of_list graph_list in
   for i = 0 to n - 1 do
     let j = fst g.(i)
     and el = snd g.(i) in
     graph.(j) <- el;
   done;
-  ();;
+  ();; *)
 
 let reset_array tab el =
   for i = 0 to (Array.length tab) - 1 do
@@ -20,21 +86,9 @@ let reset_array tab el =
   done;
   ();;
 
-(* number of vertecies in a graph given as a list 
-   eg.: [(1, [2; 3]); (2, [4]); (3, [4; 5])]*)
-let rec num_of_vertecies lst n =
-  match lst with
-  | [] -> n
-  | (v, edges) :: tail ->
-    let rec pom l maxi =
-      match l with
-      | [] -> maxi
-      | h :: t -> pom t (max maxi h) in
-    num_of_vertecies tail (max n (pom edges 0))
-;;
-
-let topol graph_list =
-  let n = num_of_vertecies graph_list 0 in
+let topol (graph_list : ('a * 'a list) list) =
+  let (map_decode, map_code, n) = convert_to_map graph_list PMap.empty PMap.empty 0 in
+  (* let n = num_of_vertecies graph_list 0 in *)
   let len = List.length graph_list in
   let q = Queue.create () in
   let vertecies_to_v = Array.make (n + 1) 0 in
@@ -44,7 +98,9 @@ let topol graph_list =
   reset_array vertecies_to_v 0;
   reset_array graph [];
 
-  create_graph len graph_list graph;
+  convert graph_list map_code graph len;
+
+  (* create_graph len graph_list graph; *)
 
   (* setting the number of edges going into each vertex of the graph *)
   for i = 1 to n do
@@ -76,7 +132,9 @@ let topol graph_list =
     else
       ()
   done;
-  List.rev !answer;;
+  (* List.rev !answer;; *)
+  decode !answer [] map_decode;;
+  (* graph;; *)
 
 (* test *)
 (* let test = [(1, [2; 3]); (2, [4]); (3, [4]); (4, [5]); (5, [3])];;
